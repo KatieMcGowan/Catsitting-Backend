49d21be2e8548498d05413aef5bc1b6dbc1b93c7
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-09-13 16:30:32 -0700

    Updates res send

--- server.js
+++ server.js
@@ -9,6 +9,10 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.get("/", (req, res) => {
+  res.send("<h1>HiTail API</h1>")
+})
+
 app.use("/users", routes.users);
 app.use("/cats", routes.cats);
 app.use("/requests", routes.requests);
