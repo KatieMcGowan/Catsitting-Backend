ca8ea5bbca0b51ab92ee51a65fa824a255b2f71b
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-09-13 17:02:15 -0700

    Updates server.js

--- server.js
+++ server.js
@@ -6,8 +6,9 @@ const PORT = process.env.PORT || "https://serene-anchorage-09864.herokuapp.com";
 const app = express();
 
 //MIDDLEWARE
-app.use(cors());
 app.use(express.json());
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("<h1>HiTail API</h1>")
