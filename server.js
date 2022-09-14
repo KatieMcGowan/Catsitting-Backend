9f2298c1938ae8300c42a3ab85bd45a5ea6a497a
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-09-13 17:15:35 -0700

    More debugging

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 //DEPENDENCIES
+require('dotenv').config()
 const express = require("express");
-const routes = require("./routes");
 const cors = require("cors");
-const PORT = process.env.PORT || "https://serene-anchorage-09864.herokuapp.com";
 const app = express();
+
+const PORT = process.env.PORT || "https://serene-anchorage-09864.herokuapp.com";
+const routes = require("./routes");
 
 //MIDDLEWARE
 app.use(express.json());
-
 app.use(cors());
 
 app.get("/", (req, res) => {
