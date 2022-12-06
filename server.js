fda0158bffba6f99d81f1664e5ee7c90c18f341f
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-12-06 12:42:11 -0800

    More tinkering

--- server.js
+++ server.js
@@ -8,12 +8,12 @@ const routes = require("./routes");
 
 //MIDDLEWARE
 app.use(express.json());
-app.use(cors());
+// app.use(cors());
 
-// app.use(cors({
-  // credentials: true,
-//   origin: process.env.HITAIL_CORS_DOMAIN,
-// }))
+app.use(cors({
+  credentials: true,
+  origin: process.env.HITAIL_CORS_DOMAIN,
+}))
 
 //CORS
 app.use((req, res, next) => {
