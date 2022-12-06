25849f2f3d3e4dfb81362b385b65cab99761ae89
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-12-06 09:45:09 -0800

    Modifies CORS

--- server.js
+++ server.js
@@ -3,7 +3,6 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
 const PORT = process.env.PORT || process.env.URL;
 const routes = require("./routes");
 
@@ -11,10 +10,24 @@ const routes = require("./routes");
 app.use(express.json());
 // app.use(cors());
 
-app.use(cors({
-  credentials: true,
-  origin: process.env.HITAIL_CORS_DOMAIN,
-}))
+// app.use(cors({
+//   credentials: true,
+//   origin: process.env.HITAIL_CORS_DOMAIN,
+// }))
+
+//CORS
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", process.env.HITAIL_CORS_DOMAIN);
+  res.setHeader(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>HiTail API</h1>")
@@ -27,5 +40,3 @@ app.use("/messages", routes.messages);
 
 //LISTENER
 app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-
-console.log('test')
