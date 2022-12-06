559cfe6ce572c70fb98556a5e491253513e1650c
Katie McGowan <katiemcgowan@Matts-MacBook-Air.local>
2022-12-06 13:42:16 -0800

    Makes more adjustments

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 const express = require("express");
 const cors = require("cors");
 const app = express();
-const PORT = process.env.PORT || process.env.URL;
+const PORT = process.env.PORT || 4000;
 const routes = require("./routes");
 
 //MIDDLEWARE
@@ -12,22 +12,22 @@ app.use(cors());
 
 app.use(cors({
   credentials: true,
-  origin: process.env.HITAIL_CORS_DOMAIN,
+  origin: "https://peaceful-violetblooms-486731.onrender.com",
 }))
 
 //CORS
-// app.use((req, res, next) => {
-//   res.setHeader("Access-Control-Allow-Origin", "https://hitail.onrender.com/");
-//   res.setHeader(
-//     "Access-Control-Allow-Headers",
-//     "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
-//     );
-//   res.setHeader(
-//     "Access-Control-Allow-Methods",
-//     "GET, POST, PUT, DELETE, PATCH, OPTIONS"
-//   );
-//   next();
-// });
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://hitail.onrender.com/");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content, Accept, Content-Type, Authorization"
+    );
+  res.header(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, PATCH, OPTIONS"
+  );
+  next();
+});
 
 app.get("/", (req, res) => {
   res.send("<h1>HiTail API</h1>")
